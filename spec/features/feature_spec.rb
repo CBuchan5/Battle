f3bbfad59730bac645586ec50a52d987b683314a
Charlotte Buchan <cebuc@talktalk.net>
2022-02-10 15:32:43 +0000

    still on step 20

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -20,14 +20,20 @@ feature 'Enter names' do
 end
 
 feature "play game", type: :feature do 
+  scenario "display players hitpoints" do
+    visit '/play'
+    fill_in :player_one, with: "Charlotte"
+    fill_in :player_two, with: "Eddie"
+    click_button 'Submit'
+    expect(page).to have_content "Eddie: 50hp"
+    expect(page).to have_content "Charlotte: 50hp"
+  end
   scenario "display battle option buttons" do
-  visit '/play'
-  expect(page).to have_content "opponent hp"
-  expect(page).to have_content "my hp"
-  expect(page).to have_selector(:link_or_button, "Attack!")
-  expect(page).to have_selector(:link_or_button, "Paralyse")
-  expect(page).to have_selector(:link_or_button, "Sleep, zzz")
-  expect(page).to have_selector(:link_or_button, "Poison @_@") 
-  expect(page).to have_selector(:link_or_button, "Heal <3") 
+    visit '/play'
+    expect(page).to have_selector(:link_or_button, "Attack!")
+    expect(page).to have_selector(:link_or_button, "Paralyse")
+    expect(page).to have_selector(:link_or_button, "Sleep, zzz")
+    expect(page).to have_selector(:link_or_button, "Poison @_@") 
+    expect(page).to have_selector(:link_or_button, "Heal <3") 
   end
 end
